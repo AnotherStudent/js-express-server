37a6ecfaf0387321cbd39e835ae784999a254a2c
errorcalc <errorsoft@mail.ru>
2018-03-20 13:24:02 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { get } = require('axios');
+
+const PORT = 9850;
+const app = express();
+const URL = 'https://kodaktor.ru/j/users';
+
+app
+  .get('/', r => r.res.send('lol kek chebureck!\n'))
+  .get('/hello/', r => r.res.end('Hello word!\n'))
+  .get('/hello/:name', r => r.res.end('Hello ' + r.params.name + '!\n'))
+
+  // дыра в безопсности
+  .get('/calc/:e', r => r.res.end(r.params.e + ' = ' + eval(r.params.e) + '\n'))
+
+  .get('/users/', async r => {
+  	const{data: {users: items}} = await get(URL);
+  	r.res.render('list', {title: 'Login list', items});
+  })
+
+  .use(r => r.res.status(404).end('sorry :(\n'))
+  .use((e, r, res, n) => res.status(500).end(`Error: ${e}`))
+  .set('view engine', 'pug')
+  .listen(process.env.PORT || PORT, () => console.log(process.pid));
